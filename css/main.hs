1d2bc03ba08f136a19a61c5943dcb0b6e4c715d1
Levi Schuck <levi.schuck@gmail.com>
2014-02-09 23:25:32 -0700

    Adding a style that works more aesthetically.

--- css/main.hs
+++ css/main.hs
@@ -13,13 +13,18 @@ main = T.putStr $ renderWith compact $ do
     (img <> object <> embed) ? do
         maxWidth $ pct 100
     body ? do
-        width $ px 600
-        sym2 margin 0 auto
         backgroundColor "#F7F7F7"
         basetextcolor
         backgroundImage $ url "/images/dots.svg"
         fontSize $ pct 100
         "line-height" -: "1.25"
+        padding 0 0 0 0
+        margin 0 0 0 0
+    "#bodyWrap" ? do
+        maxWidth $ em 40
+        sym2 margin 0 auto
+        -- backgroundColor white
+        boxShadow 0 (px 1) (px 6) (rgba 0 0 0 15)
     "#header" ? do
         borderBottom solid (px 2) black
         sym2 padding (px 12) 0
@@ -51,37 +56,50 @@ main = T.putStr $ renderWith compact $ do
         secondcolor
         fontSize $ px 14
         fontStyle italic
-    (h1 <> h2 <> h3) ? do
+    (h1 <> h2 <> h3 <> h4 <> h5) ? do
         marginTop 0
         marginBottom 0
+        marginLeft $ px 14
         color "#222A40"
+    (p 
+        <> "#footer"
+        <> "#header"
+        <> ".info"
+        ) ? do
+        paddingLeft $ em 1
+        paddingRight $ em 1
     let modular = [(h1, 2.625, 1.27976), (h2, 2, 1.25), (h3, 1.5, 1.45833)]
     forM_ modular $ \(tag, size, pad) -> tag ? do
         fontSize $ em size
-        sym2 padding (em pad) 0
+        paddingTop $ em pad
+        paddingBottom $ em pad
     blockquote ? do
-        borderLeft solid (px 4) gray
-        paddingLeft $ em 1
-        marginLeft $ em 1
+        padding (em 2) (em 2) (em 1) (em 3)
+        textAlign $ alignSide sideRight
+        backgroundColor $ "#D82545"
+        color white
+        margin 0 0 0 0
+        fontSize (pct 125)
+    ".figure" ? do
+        textAlign $ alignSide sideCenter
     figure ? do
         marginLeft 0
         marginRight 0
     figcaption ? do
         textAlign $ alignSide sideCenter
-    let defSpace = 11
+    let defSpace = 8
     dl ? do -- Definition List
-        marginBottom $ em 1
+        backgroundColor "#68A8C3"
+        color white
+        paddingTop $ em 1
+        paddingBottom $ em 1
         dt ? do -- Definition Term
-            textTransform uppercase
-            display inline
-            fontWeight bold
+            color "#3A6073"
             float floatLeft
+            fontWeight bold
             width $ em defSpace
             clear clearLeft
-            "text-overflow" -: "ellipsis"
-            "white-space" -: "nowrap"
             textAlign $ alignSide sideRight
-            overflow hidden
         dd ? do -- Definition Description
             marginLeft $ em $ 1 + defSpace
 
